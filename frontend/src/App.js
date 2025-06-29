1b335e64c1e2b9d57f26d6fb6032cc6f96e5530b
hawesd04 <hawesd04@gmail.com>
2025-06-29 16:39:38 -0400

    initial attempt, first hallway and door

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Hallway from './Hallway';
+import { Typography } from '@mui/material';
 import './App.css';
 
 function App() {
+
+  const arr = [1,2,3,4,5,6,7,8]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>
+          Pogchat House
+        </h2>
+        <Hallway
+          rooms = {arr}
+          />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/Hallway.js
@@ -0,0 +1,31 @@
+import './Hallway.css';
+import { useState } from 'react';
+import { Box, Typography } from '@mui/material'
+
+const Hallway = ({rooms}) => {
+
+    const assets = [
+        './HallwayAssets/Hallway1.png',
+        './HallwayAssets/Hallway2.png',
+        './HallwayAssets/Hallway3.png',
+        './HallwayAssets/Hallway4.png',
+        './HallwayAssets/Hallway5.png',
+        './HallwayAssets/Hallway6.png',
+    ];
+
+    const bg = Math.floor(rooms.length/5);
+    const numDoors = Math.floor(rooms.length%5)
+    console.log("bg index set to:" + bg);
+    console.log("remainder set to:" + numDoors);
+
+    return(
+        <Box>
+            <div className="hallways">
+                <img className="hallway-image" src={assets[bg]}></img>
+                <img className="door-image" src={"./HallwayAssets/door.png"}></img>
+            </div>
+        </Box>
+    );
+};
+
+export default Hallway
