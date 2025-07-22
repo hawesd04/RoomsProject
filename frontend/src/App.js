4a3f5531926a2a11d78013bbca4278e6ad6650a0
hawesd04 <hawesd04@gmail.com>
2025-07-21 23:28:58 -0400

    Some comments on App.js and Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import Hallway from './Hallway';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Home';
-import { Typography } from '@mui/material';
 import './App.css';
 import Room from './Room';
-
+/*
+  This is the main launcher for the full application. It sets up all web routes you can navigate to.
+*/
 function App() {
 
-  // return for main app, includes all possible web routes
+  // Return for main app. Includes all possible web-routes
   return (
     <BrowserRouter>
       <Routes>
+        {/* Route for the Home element*/}
         <Route path="/" element={<Home />} />
+        {/* Route for each room element*/}
         <Route path="/room/:id" 
           element={<Room />} />
+        {/* Route for the room-creator element*/}
+        <Route path="/create-new" element={<RoomCreator />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,13 +4,37 @@ import { useState, useEffect } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Button, Typography } from '@mui/material'
 import './Home.css';
+/*
+  Website Homepage, this is where you view the hallways and all parts of the web page!
+  This is essentially "the top" of the program, the file highest up in the heirarchy,
+  besides App which just holds the routes.
 
+  Includes the text at the top labeled 'Pogchat House'
+  Then an array map of hallway objects (Object Oriented Programming) each
+  containing 5 of the doors from the database
+
+  The database array is spliced into sets of 5 doors and they're sent individually
+  into each hallway. The number of hallways increases with number of doors in the database.
+
+  DevMode useState boolean toggles devmode options like image editing, text editing, and data removal
+  CorrectPass useState boolean saves that you entered the correct password once, so you don't have to
+  enter it every time you toggle devmode afterwards.
+*/
 function Home() {
 
+  // States are a type of variable in React that hold and modify constants (Data that cannot change)
+  // the data itself is the first (blue) variable, and the method to update it is the second (yellow) 
+  // the initial state of value of that variable is defined in useState([CONTENTS])
+
+  // For instance, the initial state of the doors is an empty string
+  // the initial state of devMode is FALSE, so its off by default
   const [initialDoors, setInitialDoors] = useState('');  
   const [devMode, setDevMode] = useState(false);
   const [correctPass, setCorrectPass] = useState(false);
 
+  /*
+    Grabs the data from the database and sets the initial door list with each door
+  */
   useEffect(() => {
     axios.get("http://localhost:5000/api/data")
     .then(response => {
@@ -19,7 +43,16 @@ function Home() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Create a function to update a specific door
+
+
+  /*
+    Sends the updated data back to the backend from wherever this
+    method is called from. Basically, if you update a name or profile picture,
+    the updated version is sent here and populated properly from the top down.
+
+    This method is called deeper in hallway.js and others to update content
+    in the backend
+  */
   const updateDoor = (doorId, updatedData) => {
       setInitialDoors(prevDoors => 
           prevDoors.map(door => 
@@ -30,9 +63,12 @@ function Home() {
       );
   };
 
-  console.log(initialDoors)
 
+  /*
+    Toggles on and off Dev Mode!
 
+    Includes passcode system
+  */
   const handleDevModeToggle = () => {
     if (correctPass === true) {
       if (devMode === true) {
@@ -45,7 +81,6 @@ function Home() {
     else {
       let userInput = prompt("Enter the dev passcode: ");
       if (userInput !== null && userInput !== "") {
-        // pix if you see this thats really damn funny lmao
         if (userInput == "temp") {
           setDevMode(true);
           setCorrectPass(true);
@@ -64,12 +99,15 @@ function Home() {
   // const handleRemModeToggle = () => {
   // }
 
-  
+  /*
+    Method that will reroute to the room creator whenever I make it
+  */
   const handleCreateRoom = () => {
+    //NAVIGATE TO room-creator
   }
 
 
-
+  // Splits the door array into defined chunks
   function chunkArray(arr, chunkSize) {
     const chunks = [];
     for (let i = 0; i < arr.length; i += chunkSize) {
@@ -78,11 +116,17 @@ function Home() {
     }
     return chunks;
   }
+
+  // calls chunkarray with size of 5, (5 doors per hallway)
   const doorsInHall = chunkArray(initialDoors, 5)
   console.log(doorsInHall)
 
 
-
+  /* 
+  RETURN METHOD for HOME 
+  
+  Everything the user can VISUALLY see in the frontend. 
+  */
   return (
           <div className="App">
             <div className="container">
